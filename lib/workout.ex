8f669c14534dc8ad05baca4e73206587dfd1c71e
Eric Rasmussen <ericrasmussen@gmail.com>
2020-04-28 10:46:07 -0700

    no need for () in typespecs after all

--- lib/workout.ex
+++ lib/workout.ex
@@ -18,15 +18,15 @@ defmodule Workout do
   that they are distinct type vars and not interchangeable.
   """
 
-  @type a() :: any()
-  @type b() :: any()
+  @type a :: any
+  @type b :: any
 
   # fold takes an accumulator of some type `b`, a list of values of
   # some type `a`, and a function of `a` -> `b` -> `b`. It returns a list
   # containing values of type `b`.
   # example: fold("", ["oh", "no"], fn(a, b) -> a <> b end) == "ohno"
   # see the tests for more examples
-  @spec fold(b(), list(a()), (a(), b() -> b())) :: b()
+  @spec fold(b, list(a), (a, b -> b)) :: b
   def fold(accum, [], _func) do
     []
   end
@@ -37,7 +37,7 @@ defmodule Workout do
 
   # some of the following functions benefit from having an easy way to
   # append an item to a list.
-  @spec append(a(), list(a())) :: list(a())
+  @spec append(a, list(a)) :: list(a)
   def append(x, items) do
     []
   end
@@ -46,14 +46,14 @@ defmodule Workout do
   # map takes a list of items (e.g. [1, 2, 3]) and a function
   # (e..g fn a -> a + 1 end), and returns a new list with the function applied
   # to each element (e.g. [2, 3, 4])
-  @spec map(list(a()), (a() -> b())) :: list(b())
+  @spec map(list(a), (a -> b)) :: list(b)
   def map(items, func) do
     items
   end
 
   # filter should also be defined in terms of fold! it's a theme!
   # no I don't know why I'm still using exclamations!
-  @spec filter(list(a()), (a() -> boolean())) :: list(a())
+  @spec filter(list(a), (a -> boolean)) :: list(a)
   def filter(items, pred) do
     items
   end
@@ -61,7 +61,7 @@ defmodule Workout do
   # you can try defining `any` in terms of `filter` or `all`.
   # it should return true if `pred(item)` is true for at least one item in the
   # given `items` list, else false
-  @spec any(list(a()), (a() -> boolean())) :: boolean()
+  @spec any(list(a), (a -> boolean)) :: boolean
   def any(items, pred) do
     true
   end
@@ -69,7 +69,7 @@ defmodule Workout do
   # there are also mutiple ways to define `all`. you can try defining it in
   # terms of `any`. returns true if `pred(item)` holds true for each
   # `item` in the given list `items`, else false
-  @spec all(list(a()), (a() -> boolean())) :: boolean()
+  @spec all(list(a), (a -> boolean)) :: boolean
   def all(items, pred) do
     true
   end
@@ -77,7 +77,7 @@ defmodule Workout do
 
   # max should return the largest item (using the built-in > operator) in a
   # given list. this should be defined in terms of `fold`.
-  @spec max(list(a())) :: a()
+  @spec max(list(a)) :: a
   def max([]) do
     []
   end
@@ -88,7 +88,7 @@ defmodule Workout do
 
   # min should return the smallest item (using the built-in < operator) in a
   # given list. this should be defined in terms of `fold`.
-  @spec min(list(a())) :: a()
+  @spec min(list(a)) :: a
   def min([]) do
     []
   end
@@ -99,14 +99,14 @@ defmodule Workout do
 
   # `len` should count the number of items in the given list. this should be
   # defined in terms of `fold`.
-  @spec len(list(a())) :: integer()
+  @spec len(list(a)) :: integer
   def len(items) do
     0
   end
 
   # splits one list into two. the first list contains all of the elements
   # where `pred(element)` is true, and the second list contains the rest
-  @spec split_by(list(a()), (a() -> boolean())) :: { list(a()), list(a()) }
+  @spec split_by(list(a), (a -> boolean)) :: { list(a), list(a) }
   def split_by(items, pred) do
     { [], [] }
   end
@@ -114,7 +114,7 @@ defmodule Workout do
   # this should be defined in terms of either fold or filter. don't worry about
   # efficiency. to keep it simple, you can sort by < (e.g. if you have a list
   # [1, 3, 2], it should be sorted as [1, 2, 3])
-  @spec insertion_sort(list(a())) :: list(a())
+  @spec insertion_sort(list(a)) :: list(a)
   def insertion_sort(items) do
     []
   end
